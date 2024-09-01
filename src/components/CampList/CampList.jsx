de2c3e5389bf30c747e3d5a543b20411f8b00a14
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-09-01 20:11:54 +0300

    pagination added

--- src/components/CampList/CampList.jsx
+++ src/components/CampList/CampList.jsx
@@ -1,20 +1,45 @@
 /* eslint-disable no-unused-vars */
 import css from './CampList.module.css';
 import { useSelector } from 'react-redux';
-import { selectCamps, selectFilteredCamps } from '../../redux/transports/selectors.js';
+import { selectFilteredCamps } from '../../redux/transports/selectors.js';
 import CampListItem from '../CampListItem/CampListItem';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { selectOptionFilters } from '../../redux/filters/selectors';
+
 export default function CampList() {
-    const camps = useSelector(selectFilteredCamps)
-    const equipment = useSelector(selectOptionFilters)
+    const camps = useSelector(selectFilteredCamps);
+    const equipment = useSelector(selectOptionFilters);
+    const [itemsPerPage] = useState(2); 
+    const [currentPage, setCurrentPage] = useState(1); 
+    const listRef = useRef(null); 
+
+    const totalPages = Math.ceil(camps.length / itemsPerPage);
+
+    const displayedCamps = camps.slice(0, currentPage * itemsPerPage);
+
+    const loadMore = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+            setTimeout(() => {
+                listRef.current.scrollIntoView({ behavior: 'smooth' });
+            }, 100); 
+        }
+    };
+
     return (
-        <ul className={css.list}>
-            {camps.map(camp => (
-                <li className={css.item} key={camp.id}>
-                    <CampListItem camp={camp} />
-                </li>
-            ))}
-        </ul>
+        <div>
+            <ul className={css.list} >
+                {displayedCamps.map(camp => (
+                    <li ref={listRef} className={css.item} key={camp.id}>
+                        <CampListItem camp={camp} />
+                    </li>
+                ))}
+            </ul>
+            {currentPage < totalPages && (
+                <button onClick={loadMore} className={css.loadMoreBtn}>
+                    Load More
+                </button>
+            )}
+        </div>
     );
 }
--- src/components/CampListItem/CampListItem.jsx
+++ src/components/CampListItem/CampListItem.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import css from './CampListItem.module.css'
+import css from './CampListItem.module.css';
 import { NavLink } from 'react-router-dom';
 import sprite from '../../assets/sprite.svg';
 import { useDispatch, useSelector } from 'react-redux';
@@ -28,120 +28,79 @@ export default function CampListItem({
       AC,
       gallery,
     },
-  }) {
+}) {
     const imgPaths = gallery.map((img) => img.thumb);
     const path = imgPaths[0];
-  const dispatch = useDispatch()
-  const favoriteItems = useSelector(selectFavs);
-  const isFavorite = favoriteItems.some((item) => item.id === id);
+    const dispatch = useDispatch();
+    const favoriteItems = useSelector(selectFavs);
+    const isFavorite = favoriteItems.some((item) => item.id === id);
+
     const categories = [
-      {
-        name: 'AC',
-        value: AC,
-        icon: '#icon-wind',
-      },
-      {
-        name: 'Transmission',
-        value: transmission,
-        icon: '#icon-diagram',
-      },
-      {
-        name: 'Kitchen',
-        value: kitchen,
-        icon: '#icon-cup',
-      },
-      {
-        name: 'TV',
-        value: TV,
-        icon: '#icon-tv',
-      },
-      {
-        name: 'Bathroom',
-        value: bathroom,
-        icon: '#icon-droplet',
-      },
-      {
-        name: 'Radio',
-        value: radio,
-        icon: '#icon-radio',
-      },
-      {
-        name: 'Refrigerator',
-        value: refrigerator,
-        icon: '#icon-frigo',
-      },
-      {
-        name: 'Microwave',
-        value: microwave,
-        icon: '#icon-microwave',
-      },
-      {
-        name: 'Gas',
-        value: gas,
-        icon: '#icon-engine',
-      },
-      {
-        name: 'Water',
-        value: water,
-        icon: '#icon-water',
-      },
-      {
-        name: 'Engine',
-        value: engine,
-        icon: '#icon-engine',
-      },
+        { name: 'AC', value: AC, icon: '#icon-wind' },
+        { name: 'Transmission', value: transmission, icon: '#icon-diagram' },
+        { name: 'Kitchen', value: kitchen, icon: '#icon-cup' },
+        { name: 'TV', value: TV, icon: '#icon-tv' },
+        { name: 'Bathroom', value: bathroom, icon: '#icon-droplet' },
+        { name: 'Radio', value: radio, icon: '#icon-radio' },
+        { name: 'Refrigerator', value: refrigerator, icon: '#icon-frigo' },
+        { name: 'Microwave', value: microwave, icon: '#icon-microwave' },
+        { name: 'Gas', value: gas, icon: '#icon-engine' },
+        { name: 'Water', value: water, icon: '#icon-water' },
+        { name: 'Engine', value: engine, icon: '#icon-engine' },
     ];
-  
+
     const activeCategories = categories.filter((category) => category.value);
+
     const handleFavoriteToggle = () => {
-      if (isFavorite) {
-        dispatch(removeFavorite({ id }));
-      } else {
-        dispatch(addFavorite({ name, id, price, rating, location, reviews, description, transmission, bathroom, TV, radio, refrigerator, microwave, gas, water, engine, kitchen, AC, gallery }));
-      }
+        if (isFavorite) {
+            dispatch(removeFavorite({ id }));
+        } else {
+            dispatch(addFavorite({ name, id, price, rating, location, reviews, description, transmission, bathroom, TV, radio, refrigerator, microwave, gas, water, engine, kitchen, AC, gallery }));
+        }
     };
+
     return (
-      <div className={css.container}>
-        <img className={css.img} src={path} alt="photo" />
-        <div className={css.contentSide}>
-          <div className={css.mainInfo}>
-            <h2 className={css.name}>{name}</h2>
-            <div className={css.mainInfoWrapper}>
-              <p className={css.price}>€{price}.00</p>
-              <svg onClick={() => dispatch(handleFavoriteToggle)} width="26" height="24" className={css.icon}>
-              <use xlinkHref={`${sprite}${isFavorite ? '#icon-favPressed' : '#icon-fav'}`}></use>
-              </svg>
+        <div className={css.container}>
+            <img className={css.img} src={path} alt="photo" />
+            <div className={css.contentSide}>
+                <div className={css.mainInfo}>
+                    <h2 className={css.name}>{name}</h2>
+                    <div className={css.mainInfoWrapper}>
+                        <p className={css.price}>€{price}.00</p>
+                        <svg onClick={handleFavoriteToggle} width="26" height="24" className={css.icon}>
+                            <use xlinkHref={`${sprite}${isFavorite ? '#icon-favPressed' : '#icon-fav'}`}></use>
+                        </svg>
+                    </div>
+                </div>
+                <div className={css.secInfo}>
+                    <div className={css.secInfoWrapper}>
+                        <svg width="16" height="16" className={css.icon}>
+                            <use xlinkHref={`${sprite}#icon-starPressed`}></use>
+                        </svg>
+                        <p className={css.text}>
+                            {rating} ({reviews.length} Reviews)
+                        </p>
+                    </div>
+                    <div className={css.secInfoWrapper}>
+                        <svg width="16" height="16" className={css.iconMap}>
+                            <use xlinkHref={`${sprite}#iconMap`}></use>
+                        </svg>
+                        <p className={css.text}>{location}</p>
+                    </div>
+                </div>
+                <p>{description.split(' ').slice(0, 10).join(' ') + '...'}</p>
+                <div className={css.categories}>
+                    {activeCategories.map((option) => (
+                        <span key={option.name} className={css.option}>
+                            <svg width="32" height="32" className={css.iconCatalog}>
+                                <use xlinkHref={`${sprite}${option.icon}`}></use>
+                            </svg>
+                            {option.name}
+                        </span>
+                    ))}
+                </div>
+                <NavLink to={`/catalog/${id}`} className={css.btn}>Show More</NavLink>
             </div>
-          </div>
-          <div className={css.secInfo}>
-            <div className={css.secInfoWrapper}>
-              <svg width="16" height="16" className={css.icon}>
-                <use xlinkHref={`${sprite}#icon-starPressed`}></use>
-              </svg>
-              <p className={css.text}>
-                {rating}({reviews.length} Reviews)
-              </p>
-            </div>
-            <div className={css.secInfoWrapper}>
-              <svg width="16" height="16" className={css.iconMap}>
-                <use xlinkHref={`${sprite}#iconMap`}></use>
-              </svg>
-              <p className={css.text}>{location}</p>
-            </div>
-          </div>
-          <p>{description.split(' ').slice(0, 10).join(' ') + '...'}</p>
-          <div className={css.categories}>
-            {activeCategories.map((option) => (
-                <span key={option.name} className={css.option}>
-                  <svg width="32" height="32" className={css.iconCatalog}>
-                    <use xlinkHref={`${sprite}${option.icon}`}></use>
-                  </svg>
-                  {option.name}
-                </span>
-          ))}
-          </div>
-          <NavLink to={`/catalog/${id}`} className={css.btn}>Show More</NavLink>
         </div>
-      </div>
     );
-  }
+}
